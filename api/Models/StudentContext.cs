6c9a26a2f7bb5f93bbcd62867382f7049a3fa61b
Erik Pettis <erik@riversagile.com>
2018-04-02 15:31:15 -0400

    Update gulpfile tasks

--- api/Models/StudentContext.cs
+++ api/Models/StudentContext.cs
@@ -4,10 +4,8 @@ namespace api.Models
 {
 	public class StudentContext : DbContext
 	{
-		public StudentContext(DbContextOptions<StudentContext> options) : base(options)
-		{
-			this.Database.EnsureCreated();
-		}
+		public StudentContext(DbContextOptions<StudentContext> options) : base(options) { }
+
 		public DbSet<Student> Students { get; set; }
 	}
 }
