71884c035b93bf8a45ac3a59dd61d00991d8fb94
William Moore <moore.william.d@gmail.com>
2018-04-11 09:05:33 -0400

    This will communicate with the API authentication endpoint.'

--- web/ClientApp/src/app/services/authentication.service.ts
+++ web/ClientApp/src/app/services/authentication.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../models/user.model';
 
 @Injectable()
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  login(email: string, password: string) {
+    return this.http.post<User>('/api/login', { email, password })
+      // this is just the HTTP call
+      // we still need to handle the reception of the token
+      .shareReplay();
+  }
 }
