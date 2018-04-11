c163bcc907d5d7606169931e195a711fece12575
William Moore <moore.william.d@gmail.com>
2018-04-11 13:07:28 -0400

    New static assets and styling rules.

--- web/ClientApp/src/app/app-routing.module.ts
+++ web/ClientApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { PaymentsRefundsComponent } from './components/payments-refunds/payments-refunds.component';
 
@@ -9,7 +10,8 @@ const routes: Routes = [
   // { path: '', component: HomeComponent, pathMatch: 'full' },
   // { path: 'counter', component: CounterComponent },
   // { path: 'fetch-data', component: FetchDataComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'payments-refunds', component: PaymentsRefundsComponent },
 ];
