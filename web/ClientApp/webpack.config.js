46ce2c403bece17a6dca330e9fbf45d34f585c1f
Erik Pettis <erik@riversagile.com>
2018-04-13 13:29:56 -0400

    Install ts-loader

--- web/ClientApp/webpack.config.js
+++ web/ClientApp/webpack.config.js
@@ -5,6 +5,18 @@ module.exports = {
     app: './src/main.ts',
     polyfills: './src/polyfills.ts'
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
