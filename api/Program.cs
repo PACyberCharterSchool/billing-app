cdb3f9fa950f587509ff5d7a32e2d893f3964d97
Erik Pettis <erik@riversagile.com>
2018-04-17 11:07:05 -0400

    Set bind URL

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,16 +8,17 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace api
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			BuildWebHost(args).Run();
+		}
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+		public static IWebHost BuildWebHost(string[] args) =>
+						WebHost.CreateDefaultBuilder(args)
+								.UseStartup<Startup>()
+								.UseUrls("http://0.0.0.0:5000")
+								.Build();
+	}
 }
