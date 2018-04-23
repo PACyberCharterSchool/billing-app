618f3c89c68fa471e6296ddece8b9dc46e254584
Erik Pettis <erik@riversagile.com>
2018-04-23 10:47:21 -0400

    Add CreateOrUpdate to SchoolDistrictRepository

--- api.Tests/Models/SchoolDistrictRepository.Tests.cs
+++ api.Tests/Models/SchoolDistrictRepository.Tests.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 using NUnit.Framework;
 
@@ -86,6 +87,80 @@ namespace api.Tests.Models
 			Assert.That(actual[0].Name, Is.EqualTo(districts[1].Name));
 			Assert.That(actual[1].Name, Is.EqualTo(districts[2].Name));
 			Assert.That(actual[2].Name, Is.EqualTo(districts[0].Name));
+		}
+
+		[Test]
+		public void CreateOrUpdateWithNewObjectCreates()
+		{
+			var district = new SchoolDistrict
+			{
+				Name = "Bob",
+				Rate = 1.0m,
+				AlternateRate = 1.0m,
+			};
+
+			var result = _uut.CreateOrUpdate(district);
+			Assert.That(result, Is.EqualTo(district));
+
+			var actual = _context.SchoolDistricts.First(d => d.Id == result.Id);
+			Assert.That(actual.Name, Is.EqualTo(district.Name));
+			Assert.That(actual.Rate, Is.EqualTo(district.Rate));
+			Assert.That(actual.AlternateRate, Is.EqualTo(district.AlternateRate));
+		}
+
+		[Test]
+		public void CreateOrUpdateWithSameObjectUpdates()
+		{
+			var id = 3;
+			var district = new SchoolDistrict
+			{
+				Id = id,
+				Name = "Bob",
+				Rate = 1.0m,
+				AlternateRate = 1.0m,
+			};
+			_context.Add(district);
+			_context.SaveChanges();
+
+
+			district.Name = "Charlie";
+			district.Rate = 2.0m;
+			district.AlternateRate = 3.0m;
+			_uut.CreateOrUpdate(district);
+
+			var actual = _context.SchoolDistricts.First(d => d.Id == id);
+			Assert.That(actual.Name, Is.EqualTo(district.Name));
+			Assert.That(actual.Rate, Is.EqualTo(district.Rate));
+			Assert.That(actual.AlternateRate, Is.EqualTo(district.AlternateRate));
+		}
+
+		[Test]
+		public void CreateOrUpdateWithDifferentObjectUpdates()
+		{
+			var id = 3;
+			var district = new SchoolDistrict
+			{
+				Id = id,
+				Name = "Bob",
+				Rate = 1.0m,
+				AlternateRate = 1.0m,
+			};
+			_context.Add(district);
+			_context.SaveChanges();
+
+			var updated = new SchoolDistrict
+			{
+				Id = id,
+				Name = "Charlie",
+				Rate = 2.0m,
+				AlternateRate = 3.0m,
+			};
+			_uut.CreateOrUpdate(updated);
+
+			var actual = _context.SchoolDistricts.First(d => d.Id == id);
+			Assert.That(actual.Name, Is.EqualTo(updated.Name));
+			Assert.That(actual.Rate, Is.EqualTo(updated.Rate));
+			Assert.That(actual.AlternateRate, Is.EqualTo(updated.AlternateRate));
 		}
 	}
 }
--- api/Models/SchoolDistrictRepository.cs
+++ api/Models/SchoolDistrictRepository.cs
@@ -8,17 +8,20 @@ namespace api.Models
 {
 	public interface ISchoolDistrictRepository
 	{
+		SchoolDistrict CreateOrUpdate(SchoolDistrict district);
 		SchoolDistrict Get(int id);
 		IList<SchoolDistrict> GetMany();
 	}
 
 	public class SchoolDistrictRepository : ISchoolDistrictRepository
 	{
+		private readonly PacBillContext _context;
 		private readonly DbSet<SchoolDistrict> _schoolDistricts;
 		private readonly ILogger<SchoolDistrictRepository> _logger;
 
 		public SchoolDistrictRepository(PacBillContext context, ILogger<SchoolDistrictRepository> logger)
 		{
+			_context = context;
 			_schoolDistricts = context.SchoolDistricts;
 			_logger = logger;
 		}
@@ -26,5 +29,30 @@ namespace api.Models
 		public SchoolDistrict Get(int id) => _schoolDistricts.SingleOrDefault(d => d.Id == id);
 
 		public IList<SchoolDistrict> GetMany() => _schoolDistricts.OrderBy(d => d.Name).ToList();
+
+		public SchoolDistrict CreateOrUpdate(SchoolDistrict update)
+		{
+			var district = _schoolDistricts.FirstOrDefault(d => d.Id == update.Id);
+			if (district == null)
+			{
+				_schoolDistricts.Add(update);
+				_context.SaveChanges();
+				return update;
+			}
+
+			if (district.Name != update.Name)
+				district.Name = update.Name;
+
+			if (district.Rate != update.Rate)
+				district.Rate = update.Rate;
+
+			if (district.AlternateRate != update.AlternateRate)
+				district.AlternateRate = update.AlternateRate;
+
+			_schoolDistricts.Update(district);
+			_context.SaveChanges();
+
+			return district;
+		}
 	}
 }
