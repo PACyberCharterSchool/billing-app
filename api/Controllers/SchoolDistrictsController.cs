abbe884cbd33796e003df437e56ddb0e84342971
Erik Pettis <erik@riversagile.com>
2018-04-23 11:34:03 -0400

    Add /api/schooldistricts PUT endpoint

--- api/Controllers/SchoolDistrictsController.cs
+++ api/Controllers/SchoolDistrictsController.cs
@@ -31,6 +31,7 @@ namespace api.Controllers
 		[HttpGet("{id}")]
 		[Authorize(Policy = "STD+")]
 		[ProducesResponseType(typeof(SchoolDistrictResponse), 200)]
+		[ProducesResponseType(404)]
 		public async Task<IActionResult> GetById(int id)
 		{
 			var district = await Task.Run(() => _schoolDistricts.Get(id));
@@ -55,6 +56,38 @@ namespace api.Controllers
 				districts = new List<SchoolDistrict>();
 
 			return new ObjectResult(new SchoolDistrictsResponse { SchoolDistricts = districts });
+		}
+
+		public class SchoolDistrictUpdate
+		{
+			public int Aun { get; set; }
+			public string Name { get; set; }
+			public decimal Rate { get; set; }
+			public decimal? AlternateRate { get; set; }
+		}
+
+		[HttpPut("{id}")]
+		[Authorize(Policy = "PAY+")]
+		[ProducesResponseType(typeof(ErrorsResponse), 400)]
+		[ProducesResponseType(404)]
+		public async Task<IActionResult> Update(int id, [FromBody]SchoolDistrictUpdate update)
+		{
+			if (!ModelState.IsValid)
+				return new BadRequestObjectResult(new ErrorsResponse(ModelState));
+
+			if (_schoolDistricts.Get(id) == null)
+				return NotFound();
+
+			var district = new SchoolDistrict
+			{
+				Id = id,
+				Aun = update.Aun,
+				Name = update.Name,
+				Rate = update.Rate,
+				AlternateRate = update.AlternateRate,
+			};
+			await Task.Run(() => _schoolDistricts.CreateOrUpdate(district));
+			return Ok();
 		}
 	}
 }
