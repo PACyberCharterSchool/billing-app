0913e8dfa5d19a6de2d0a09bdb743bfedff342bf
Erik Pettis <erik@riversagile.com>
2018-04-25 10:23:44 -0400

    Queue multiple imports

--- import/Program.cs
+++ import/Program.cs
@@ -64,73 +64,84 @@ namespace import
 			return sb.ToString().Substring(0, 10);
 		}
 
+		private static AutoResetEvent _processing = new AutoResetEvent(true);
+
 		private static void HandleFileChange(object source, FileSystemEventArgs e)
 		{
-			using (var tx = _context.Database.BeginTransaction())
+			try
 			{
-				try
-				{
-					var table = nameof(_context.PendingStudentStatusRecords);
-					Console.WriteLine($"Truncating {table}...");
-					_context.Database.ExecuteSqlCommand($"TRUNCATE TABLE " + table + ";");
-					_context.SaveChanges();
-					Console.WriteLine("Truncating done!");
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine($"Failed to truncate table: {ex.Message}");
-					if (ex.InnerException != null)
-						Console.WriteLine($"  Inner exception: {ex.InnerException.Message}.");
-					return;
-				}
+				_processing.WaitOne();
 
-				try
+				using (var tx = _context.Database.BeginTransaction())
 				{
-					var count = 0;
-					using (var streamReader = File.OpenText(e.FullPath))
+					try
 					{
-						var csvReader = new CsvReader(streamReader);
-						csvReader.Configuration.RegisterClassMap<StudentStatusRecordClassMap>();
-
-						var batchFilename = e.Name;
-						var batchTime = DateTime.Now;
-						var batchHash = HashBatch(batchTime, batchFilename);
-						Console.WriteLine($"Filename: {batchFilename}");
-						Console.WriteLine($"Batch time: {batchTime}");
-						Console.WriteLine($"Hash: {batchHash}");
-
-						Console.WriteLine("Reading records...");
-						var records = csvReader.GetRecords<PendingStudentStatusRecord>();
-						foreach (var record in records)
-						{
-							record.BatchTime = batchTime;
-							record.BatchFilename = batchFilename;
-							record.BatchHash = batchHash;
-
-							_context.Add(record);
-							count++;
-
-							if (count % 1000 == 0)
-								Console.WriteLine($"Read {count} records.");
-						}
-
-						Console.WriteLine($"Reading records done ({count})!");
+						var table = nameof(_context.PendingStudentStatusRecords);
+						Console.WriteLine($"Truncating {table}...");
+						_context.Database.ExecuteSqlCommand($"TRUNCATE TABLE " + table + ";");
+						_context.SaveChanges();
+						Console.WriteLine("Truncating done!");
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine($"Failed to truncate table: {ex.Message}");
+						if (ex.InnerException != null)
+							Console.WriteLine($"  Inner exception: {ex.InnerException.Message}.");
+						return;
 					}
 
-					Console.WriteLine("Writing changes to the database...");
-					_context.SaveChanges();
-					Console.WriteLine("Writing changes to the database done!");
+					try
+					{
+						var count = 0;
+						using (var streamReader = File.OpenText(e.FullPath))
+						{
+							var csvReader = new CsvReader(streamReader);
+							csvReader.Configuration.RegisterClassMap<StudentStatusRecordClassMap>();
 
-					tx.Commit();
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine($"Failed to read CSV: {ex.Message}.");
-					if (ex.InnerException != null)
-						Console.WriteLine($"  Inner exception: {ex.InnerException.Message}.");
+							var batchFilename = e.Name;
+							var batchTime = DateTime.Now;
+							var batchHash = HashBatch(batchTime, batchFilename);
+							Console.WriteLine($"Filename: {batchFilename}");
+							Console.WriteLine($"Batch time: {batchTime}");
+							Console.WriteLine($"Hash: {batchHash}");
 
-					return;
+							Console.WriteLine("Reading records...");
+							var records = csvReader.GetRecords<PendingStudentStatusRecord>();
+							foreach (var record in records)
+							{
+								record.BatchTime = batchTime;
+								record.BatchFilename = batchFilename;
+								record.BatchHash = batchHash;
+
+								_context.Add(record);
+								count++;
+
+								if (count % 1000 == 0)
+									Console.WriteLine($"Read {count} records.");
+							}
+
+							Console.WriteLine($"Reading records done ({count})!");
+						}
+
+						Console.WriteLine("Writing changes to the database...");
+						_context.SaveChanges();
+						Console.WriteLine("Writing changes to the database done!");
+
+						tx.Commit();
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine($"Failed to read CSV: {ex.Message}.");
+						if (ex.InnerException != null)
+							Console.WriteLine($"  Inner exception: {ex.InnerException.Message}.");
+
+						return;
+					}
 				}
+			}
+			finally
+			{
+				_processing.Set();
 			}
 		}
 	}
