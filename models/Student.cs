edb3aa4a052ff6eb213c779e28328f459d65ac8a
Erik Pettis <erik@riversagile.com>
2018-04-26 14:37:19 -0400

    Add Student Start and End dates

--- models/Student.cs
+++ models/Student.cs
@@ -23,6 +23,8 @@ namespace models
 		public DateTime? CurrentIep { get; set; }
 		public DateTime? FormerIep { get; set; }
 		public DateTime? NOREP { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime? EndDate { get; set; }
 		public DateTime Created { get; set; }
 		public DateTime LastUpdated { get; set; }
 
