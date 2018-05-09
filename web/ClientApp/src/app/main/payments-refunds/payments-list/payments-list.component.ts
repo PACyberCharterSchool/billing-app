a4620648d788482d155fd9f50a93bf0a552a9916
William Moore <moore.william.d@gmail.com>
2018-05-09 10:19:09 -0400

    Fake data.

--- web/ClientApp/src/app/main/payments-refunds/payments-list/payments-list.component.ts
+++ web/ClientApp/src/app/main/payments-refunds/payments-list/payments-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UtilitiesService } from '../../../services/utilities.service';
+
+import { Payment } from '../../../models/payment.model';
+
 @Component({
   selector: 'app-payments-list',
   templateUrl: './payments-list.component.html',
@@ -7,7 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentsListComponent implements OnInit {
 
-  constructor() { }
+  private payments: Payment[] = [
+    {
+      schoolDistrictName: 'Seneca Valley SD',
+      schoolDistrictAUN: '123456',
+      paymentAmt: 4300.00,
+      type: '#14534',
+      paymentDate: new Date('04/01/2018'),
+      academicYear: 2018
+    },
+    {
+      schoolDistrictName: 'Mars SD',
+      schoolDistrictAUN: '654321',
+      paymentAmt: 1800.00,
+      type: '#99999',
+      paymentDate: new Date('07/22/2018'),
+      academicYear: 2019
+    },
+    {
+      schoolDistrictName: 'Indiana SD',
+      schoolDistrictAUN: '789023',
+      paymentAmt: 40000.00,
+      type: '#99999',
+      paymentDate: new Date('07/22/2018'),
+      academicYear: 2019
+    }
+  ];
+
+  constructor(private utilitiesService: UtilitiesService) { }
 
   ngOnInit() {
   }
---
+++ web/ClientApp/src/app/models/payment.model.ts
@@ -0,0 +1,8 @@
+export class Payment {
+  schoolDistrictName: string;
+  schoolDistrictAUN: string;
+  paymentAmt: number;
+  paymentDate: Date;
+  type: string;
+  academicYear: number;
+}
