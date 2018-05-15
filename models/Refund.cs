0805462265720c7b20f33421cd568d79681d6c9b
Erik Pettis <erik@riversagile.com>
2018-05-15 17:01:24 -0400

    Create Refund Migration

--- models/Refund.cs
+++ models/Refund.cs
@@ -6,7 +6,9 @@ namespace models
 	{
 		public int Id { get; set; }
 		public decimal Amount { get; set; }
+		public string CheckNumber { get; set; }
 		public DateTime Date { get; set; }
+		public string SchoolYear { get; set; }
 		public string Username { get; set; }
 		public DateTime Created { get; set; }
 		public DateTime LastUpdated { get; set; }
