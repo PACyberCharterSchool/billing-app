68f4ff0d5c2539144776ef1054a7b4faabc8eab6
William Moore <moore.william.d@gmail.com>
2018-05-24 13:20:38 -0400

    Updated specs to make them pass.

--- web/ClientApp/src/app/main/administration/administration-school-calendar/administration-school-calendar.component.spec.ts
+++ web/ClientApp/src/app/main/administration/administration-school-calendar/administration-school-calendar.component.spec.ts
@@ -1,8 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule } from '@angular/forms';
+
+import { HttpClient, HttpHandler } from '@angular/common/http';
+
 import {
   AdministrationSchoolCalendarComponent
 } from './administration-school-calendar.component';
+
+import { NormalizeFieldNamePipe } from '../../../pipes/normalize-field-name.pipe';
+import { NormalizeFieldValuePipe } from '../../../pipes/normalize-field-value.pipe';
+import { OrderByPipe } from '../../../pipes/orderby.pipe';
+
+import { SchoolCalendarService } from '../../../services/school-calendar.service';
+import { UtilitiesService } from '../../../services/utilities.service';
+
+import { Globals } from '../../../globals';
 
 describe('AdministrationSchoolCalendarComponent', () => {
   let component: AdministrationSchoolCalendarComponent;
@@ -10,7 +23,14 @@ describe('AdministrationSchoolCalendarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdministrationSchoolCalendarComponent ]
+      declarations: [
+        AdministrationSchoolCalendarComponent,
+        NormalizeFieldNamePipe,
+        NormalizeFieldValuePipe,
+        OrderByPipe
+      ],
+      imports: [ FormsModule ],
+      providers: [ SchoolCalendarService, UtilitiesService, HttpClient, HttpHandler, Globals ]
     })
     .compileComponents();
   }));
--- web/ClientApp/src/app/services/school-calendar.service.spec.ts
+++ web/ClientApp/src/app/services/school-calendar.service.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 import { SchoolCalendarService } from './school-calendar.service';
 
 describe('SchoolCalendarService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SchoolCalendarService]
+      providers: [SchoolCalendarService, HttpClient, HttpHandler]
     });
   });
 
