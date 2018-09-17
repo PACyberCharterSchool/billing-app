fe479b4b0de8750c83f42bd059e18e49368206bd
William Moore <moore.william.d@gmail.com>
2018-09-17 14:33:06 -0400

    Accounts receivable page minimally functional.

--- web/ClientApp/src/app/main/reports-invoices/accounts-receivable-as-of/accounts-receivable-as-of.component.ts
+++ web/ClientApp/src/app/main/reports-invoices/accounts-receivable-as-of/accounts-receivable-as-of.component.ts
@@ -8,6 +8,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { UtilitiesService } from '../../../services/utilities.service';
 import { ReportsService } from '../../../services/reports.service';
 import { FileSaverService } from '../../../services/file-saver.service';
+import { AcademicYearsService } from '../../../services/academic-years.service';
 
 @Component({
   selector: 'app-accounts-receivable-as-of',
@@ -23,20 +24,35 @@ export class AccountsReceivableAsOfComponent implements OnInit {
   private isDescending: boolean;
   public searchText: string;
   public asOfDate;
+  public selectedAcademicYear: string;
+  public schoolYears: string[];
+  public selectedDownloadFormat: string;
+  public downloadFormats: string[] = [
+    'Microsoft Excel',
+    'PDF'
+  ];
+  public spinnerMsg: string;
 
   constructor(
     private ngbModalService: NgbModal,
     private ngxSpinnerService: NgxSpinnerService,
     private utilitiesService: UtilitiesService,
     private reportsService: ReportsService,
-    private fileSaverService: FileSaverService
+    private fileSaverService: FileSaverService,
+    private academicYearsService: AcademicYearsService
   ) { }
 
   ngOnInit() {
     this.skip = 0;
+    this.spinnerMsg = '';
+    this.refreshAccountsReceivableAsOfList();
+    this.schoolYears = this.academicYearsService.getAcademicYears();
+  }
+
+  public refreshAccountsReceivableAsOfList(): void {
     this.reportsService.getAccountsReceivableAsOf().subscribe(
       data => {
-        this.allReports = data['reports'];
+        this.allReports = this.reports = data['reports'];
         console.log('AccountsReceivableAsOfComponent.ngOnInit():  reports are ', data['reports']);
       },
       error => {
@@ -54,7 +70,7 @@ export class AccountsReceivableAsOfComponent implements OnInit {
   listDisplayableFields() {
     if (this.allReports) {
       const fields = this.utilitiesService.objectKeys(this.allReports[0]);
-      const rejected = ['data', 'xlsx', 'type', 'id'];
+      const rejected = ['data', 'xlsx', 'type', 'id', 'approved', 'scope'];
       return fields.filter((i) => !rejected.includes(i));
     }
   }
@@ -66,8 +82,18 @@ export class AccountsReceivableAsOfComponent implements OnInit {
     return this.utilitiesService.objectValues(selected);
   }
 
-  public displayCreateAccountsReceivableAsOfDialog(createDialogContent): void {
-    const modal = this.ngbModalService.open(createDialogContent, { centered: true });
+  public displayCreateAccountsReceivableAsOfDialog(createContent): void {
+    const modal = this.ngbModalService.open(createContent, { centered: true });
+    modal.result.then(
+      (result) => {
+      },
+      (reason) => {
+      }
+    );
+  }
+
+  public displayDownloadFormatTypeDialog(downloadContent): void {
+    const modal = this.ngbModalService.open(downloadContent, { centered: true });
     modal.result.then(
       (result) => {
       },
@@ -77,21 +103,67 @@ export class AccountsReceivableAsOfComponent implements OnInit {
   }
 
   private generateAccountsReceivableAsOfReportName(): string {
-
+    return 'Accounts_Receivable_' +
+      this.selectedAcademicYear + '_' + `${this.asOfDate.month}-${this.asOfDate.day}-${this.asOfDate.year}`;
   }
 
-  public onSubmit(): void {
+  public onCreateSubmit(): void {
     this.ngxSpinnerService.show();
+    this.spinnerMsg = 'Generating accounts receivable as of report.  Please wait...';
     this.reportsService.createAccountsReceivableAsOf(
       this.generateAccountsReceivableAsOfReportName(),
-      this.schoolYear,
+      this.selectedAcademicYear,
       this.asOfDate).subscribe(
         data => {
-
+          this.ngxSpinnerService.hide();
+          this.refreshAccountsReceivableAsOfList();
         },
         error => {
-
+          this.ngxSpinnerService.hide();
+          this.refreshAccountsReceivableAsOfList();
         }
-      )
+      );
+  }
+
+  public downloadReportByFormat(report: Report, format: string): void {
+    this.ngxSpinnerService.show();
+    this.reportsService.getReportDataByFormat(report, format.includes('Microsoft Excel') ? 'excel' : 'pdf').subscribe(
+      data => {
+        console.log('AccountsReceivableAsOfComponent.downloadReportByFormat():  data is ', data);
+        this.ngxSpinnerService.hide();
+        if (format.toLowerCase().includes('excel')) {
+          this.fileSaverService.saveInvoiceAsExcelFile(data, report);
+        } else {
+          this.fileSaverService.saveInvoiceAsPDFFile(data, report);
+        }
+      },
+      error => {
+        this.ngxSpinnerService.hide();
+        console.log('AccountsReceivableAsOfComponent.downloadReportByFormat():  error is ', error);
+      }
+    );
+  }
+
+  public setSelectedAcademicYear(year: string) {
+    this.selectedAcademicYear = year;
+  }
+
+  public displayDownloadAccountsReceivableAsOfFormatDialog(downloadReportTypeContent, report: Report): void {
+    const modal = this.ngbModalService.open(downloadReportTypeContent, { centered: true });
+    modal.result.then(
+      (result) => {
+        console.log('AccountsReceivableAsOfComponent.displayDownloadAccountsReceivableAsOfFormatDialog():  download');
+        this.downloadReportByFormat(report, this.selectedDownloadFormat);
+      },
+      (reason) => {
+      }
+    );
+  }
+
+  public onDateChanged() {
+  }
+
+  public setSelectedDownloadFormat(format: string): void {
+    this.selectedDownloadFormat = format;
   }
 }
