be4f89a37bf573130d059f16236869e070e9a42a
Erik Pettis <erik@riversagile.com>
2018-09-18 11:27:39 -0400

    Filter out students withdrawn before first day

--- models/Reporters/BulkInvoiceReporter.cs
+++ models/Reporters/BulkInvoiceReporter.cs
@@ -384,10 +384,14 @@ namespace models.Reporters
 			var districts = GetInvoiceSchoolDistricts(auns, paymentType);
 			auns = districts.Select(d => d.Aun).ToList();
 
+			var firstDay = _context.Calendars.
+				Single(c => c.SchoolYear == bulk.SchoolYear).
+				Days.Single(d => d.SchoolDay == 1).Date;
+
 			var students = GetInvoiceStudents(
 				auns,
 				bulk.Scope,
-				new DateTime(bulk.FirstYear, 7, 1),
+				firstDay,
 				bulk.AsOf.EndOfMonth());
 			var transactions = GetInvoiceTransactions(
 				auns,
