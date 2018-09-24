39bff42099339e738dc766de534c0d12ff048fff
Erik Pettis <erik@riversagile.com>
2018-09-24 15:10:57 -0400

    Use TextReader when deserializing JSON

--- models/Reporters/AccountsReceivableAsOfReporter.cs
+++ models/Reporters/AccountsReceivableAsOfReporter.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Newtonsoft.Json;
 
 using models.Common;
+using System.IO;
 
 namespace models.Reporters
 {
@@ -60,6 +61,12 @@ namespace models.Reporters
 			return scopes;
 		}
 
+		private BulkInvoice Deserialize(string data)
+		{
+			using (var tr = new StringReader(data))
+				return new JsonSerializer().Deserialize<BulkInvoice>(new JsonTextReader(tr));
+		}
+
 		private IList<AccountsReceivableAsOfSchoolDistrict> GetSchoolDistricts(
 			DateTime asOf,
 			string schoolYear,
@@ -72,7 +79,7 @@ namespace models.Reporters
 			var invoices = _context.Reports.
 				Where(r => scopes.Contains(r.Scope)).
 				Where(r => r.Type == ReportType.BulkInvoice || r.Type == ReportType.Invoice).
-				Select(r => JsonConvert.DeserializeObject<BulkInvoice>(r.Data)).
+				Select(r => Deserialize(r.Data)).
 				OrderByDescending(i => i.Prepared);
 
 			var districts = new Dictionary<int, BulkInvoiceSchoolDistrict>();
