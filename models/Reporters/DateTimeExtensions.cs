b02333f9b1eb57abebf31bf9a120e5fb9bae1885
Erik Pettis <erik@riversagile.com>
2018-09-27 14:24:56 -0400

    Reverse IsBefore logic

--- models/Reporters/DateTimeExtensions.cs
+++ models/Reporters/DateTimeExtensions.cs
@@ -1,13 +1,22 @@
 using System;
+using models.Common;
 
 namespace models.Reporters
 {
 	public static class DateTimeExtensions
 	{
-		public static bool IsAfter(this DateTime time, int month)
-			=> month >= 7 && month >= time.Month || month < 7 && month <= time.Month;
+		public static bool IsBefore(this DateTime time, Month month)
+		{
+			DateTime when;
+			if (month.FirstYear)
+				when = month.AsDateTime(time.Year).EndOfMonth();
+			else
+				when = month.AsDateTime(time.Year + 1).EndOfMonth();
 
-		public static bool IsBefore(this DateTime time, int month) => !time.IsAfter(month);
+			return time < when;
+		}
+
+		public static bool IsAfter(this DateTime time, Month month) => !time.IsBefore(month);
 
 		public static DateTime EndOfMonth(this DateTime time)
 			=> new DateTime(time.Year, time.Month, DateTime.DaysInMonth(time.Year, time.Month));
