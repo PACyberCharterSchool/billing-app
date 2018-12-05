b97855995b289780da40e2ce2e488cd06a1ecb50
Erik Pettis <erik@riversagile.com>
2018-12-05 14:30:39 -0500

    Fix list of student grade filter options

--- web/ClientApp/src/app/main/students/student-advanced-filter/student-advanced-filter.component.ts
+++ web/ClientApp/src/app/main/students/student-advanced-filter/student-advanced-filter.component.ts
@@ -1,12 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-
 import { SchoolDistrict } from '../../../models/school-district.model';
-
 import { StudentsService } from '../../../services/students.service';
 import { StudentRecordsService } from '../../../services/student-records.service';
-
 import { StudentRecord } from '../../../models/student-record.model';
-
 import { NgxSpinnerService } from 'ngx-spinner';
 import { StudentDatepickerComponent } from '../student-datepicker/student-datepicker.component';
 
@@ -17,8 +13,8 @@ import { StudentDatepickerComponent } from '../student-datepicker/student-datepi
 })
 export class StudentAdvancedFilterComponent implements OnInit {
 
-  public grades: number[] = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+  public grades: string[] = [
+    'K5F', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'
   ];
   public date: Date;
   public startDate: Date;
